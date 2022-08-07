28c732c55ae8fb9823f99dd679130334a8a7661b
aizichen <1098679804@qq.com>
2022-08-07 19:33:02 +0800

    correct code

--- dump.py
+++ dump.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # Author : AloneMonkey
@@ -34,8 +34,8 @@ DUMP_JS = os.path.join(script_dir, 'dump.js')
 
 User = 'root'
 Password = 'alpine'
-Host = 'localhost'
-Port = 2222
+Host = '127.0.0.1'
+Port = 22222
 KeyFileName = None
 
 TEMP_DIR = tempfile.gettempdir()
@@ -60,7 +60,8 @@ def get_usb_iphone():
 
     device = None
     while device is None:
-        devices = [dev for dev in device_manager.enumerate_devices() if dev.type == Type]
+        devices = [dev for dev in device_manager.enumerate_devices()
+                   if dev.type == Type]
         if len(devices) == 0:
             print('Waiting for USB device...')
             changed.wait()
@@ -86,15 +87,17 @@ def generate_ipa(path, display_name):
                 shutil.move(from_dir, to_dir)
 
         target_dir = './' + PAYLOAD_DIR
-        zip_args = ('zip', '-qr', os.path.join(os.getcwd(), ipa_filename), target_dir)
+        zip_args = ('zip', '-qr', os.path.join(os.getcwd(),
+                    ipa_filename), target_dir)
         subprocess.check_call(zip_args, cwd=TEMP_DIR)
         shutil.rmtree(PAYLOAD_PATH)
     except Exception as e:
         print(e)
         finished.set()
 
+
 def on_message(message, data):
-    t = tqdm(unit='B',unit_scale=True,unit_divisor=1024,miniters=1)
+    t = tqdm(unit='B', unit_scale=True, unit_divisor=1024, miniters=1)
     last_sent = [0]
 
     def progress(filename, size, sent):
@@ -116,7 +119,7 @@ def on_message(message, data):
             scp_from = dump_path
             scp_to = PAYLOAD_PATH + '/'
 
-            with SCPClient(ssh.get_transport(), progress = progress, socket_timeout = 60) as scp:
+            with SCPClient(ssh.get_transport(), progress=progress, socket_timeout=60) as scp:
                 scp.get(scp_from, scp_to)
 
             chmod_dir = os.path.join(PAYLOAD_PATH, os.path.basename(dump_path))
@@ -134,7 +137,7 @@ def on_message(message, data):
 
             scp_from = app_path
             scp_to = PAYLOAD_PATH + '/'
-            with SCPClient(ssh.get_transport(), progress = progress, socket_timeout = 60) as scp:
+            with SCPClient(ssh.get_transport(), progress=progress, socket_timeout=60) as scp:
                 scp.get(scp_from, scp_to, recursive=True)
 
             chmod_dir = os.path.join(PAYLOAD_PATH, os.path.basename(app_path))
@@ -149,6 +152,7 @@ def on_message(message, data):
         if 'done' in payload:
             finished.set()
     t.close()
+
 
 def compare_applications(a, b):
     a_is_running = a.pid != 0
@@ -168,7 +172,6 @@ def compare_applications(a, b):
 
 def cmp_to_key(mycmp):
     """Convert a cmp= function into a key= function"""
-
     class K:
         def __init__(self, obj):
             self.obj = obj
@@ -207,9 +210,11 @@ def list_applications(device):
     applications = get_applications(device)
 
     if len(applications) > 0:
-        pid_column_width = max(map(lambda app: len('{}'.format(app.pid)), applications))
+        pid_column_width = max(
+            map(lambda app: len('{}'.format(app.pid)), applications))
         name_column_width = max(map(lambda app: len(app.name), applications))
-        identifier_column_width = max(map(lambda app: len(app.identifier), applications))
+        identifier_column_width = max(
+            map(lambda app: len(app.identifier), applications))
     else:
         pid_column_width = 0
         name_column_width = 0
@@ -218,14 +223,16 @@ def list_applications(device):
     header_format = '%' + str(pid_column_width) + 's  ' + '%-' + str(name_column_width) + 's  ' + '%-' + str(
         identifier_column_width) + 's'
     print(header_format % ('PID', 'Name', 'Identifier'))
-    print('%s  %s  %s' % (pid_column_width * '-', name_column_width * '-', identifier_column_width * '-'))
+    print('%s  %s  %s' % (pid_column_width * '-',
+          name_column_width * '-', identifier_column_width * '-'))
     line_format = '%' + str(pid_column_width) + 's  ' + '%-' + str(name_column_width) + 's  ' + '%-' + str(
         identifier_column_width) + 's'
     for application in sorted(applications, key=cmp_to_key(compare_applications)):
         if application.pid == 0:
             print(line_format % ('-', application.name, application.identifier))
         else:
-            print(line_format % (application.pid, application.name, application.identifier))
+            print(line_format %
+                  (application.pid, application.name, application.identifier))
 
 
 def load_js_file(session, filename):
@@ -271,7 +278,7 @@ def open_target_app(device, name_or_bundleid):
         else:
             session = device.attach(pid)
     except Exception as e:
-        print(e) 
+        print(e)
 
     return session, display_name, bundle_identifier
 
@@ -290,15 +297,24 @@ def start_dump(session, ipa_name):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='frida-ios-dump (by AloneMonkey v2.0)')
-    parser.add_argument('-l', '--list', dest='list_applications', action='store_true', help='List the installed apps')
-    parser.add_argument('-o', '--output', dest='output_ipa', help='Specify name of the decrypted IPA')
-    parser.add_argument('-H', '--host', dest='ssh_host', help='Specify SSH hostname')
-    parser.add_argument('-p', '--port', dest='ssh_port', help='Specify SSH port')
-    parser.add_argument('-u', '--user', dest='ssh_user', help='Specify SSH username')
-    parser.add_argument('-P', '--password', dest='ssh_password', help='Specify SSH password')
-    parser.add_argument('-K', '--key_filename', dest='ssh_key_filename', help='Specify SSH private key file path')
-    parser.add_argument('target', nargs='?', help='Bundle identifier or display name of the target app')
+    parser = argparse.ArgumentParser(
+        description='frida-ios-dump (by AloneMonkey v2.0)')
+    parser.add_argument('-l', '--list', dest='list_applications',
+                        action='store_true', help='List the installed apps')
+    parser.add_argument('-o', '--output', dest='output_ipa',
+                        help='Specify name of the decrypted IPA')
+    parser.add_argument('-H', '--host', dest='ssh_host',
+                        help='Specify SSH hostname')
+    parser.add_argument('-p', '--port', dest='ssh_port',
+                        help='Specify SSH port')
+    parser.add_argument('-u', '--user', dest='ssh_user',
+                        help='Specify SSH username')
+    parser.add_argument('-P', '--password',
+                        dest='ssh_password', help='Specify SSH password')
+    parser.add_argument('-K', '--key_filename', dest='ssh_key_filename',
+                        help='Specify SSH private key file path')
+    parser.add_argument(
+        'target', nargs='?', help='Bundle identifier or display name of the target app')
 
     args = parser.parse_args()
 
@@ -331,10 +347,12 @@ if __name__ == '__main__':
         try:
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(Host, port=Port, username=User, password=Password, key_filename=KeyFileName)
+            ssh.connect(Host, port=Port, username=User,
+                        password=Password, key_filename=KeyFileName)
 
             create_dir(PAYLOAD_PATH)
-            (session, display_name, bundle_identifier) = open_target_app(device, name_or_bundleid)
+            (session, display_name, bundle_identifier) = open_target_app(
+                device, name_or_bundleid)
             if output_ipa is None:
                 output_ipa = display_name
             output_ipa = re.sub('\.ipa$', '', output_ipa)
